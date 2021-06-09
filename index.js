a6c749c9472e416d8a042fcec1728b0a16129724
miriamAtFlatiron <83549216+miriamAtFlatiron@users.noreply.github.com>
2021-06-09 13:43:24 -0400

    Three ways to solve 2d test

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+Three ways to solve: 
+
+var superbowlWin = (arr) => {
+  const results = arr.find( rec => rec.result === "W" )
+ 
+  if (!!results) {
+    return results.year
+  }
+}
+
+var superbowlWin = (arr) => {
+  const results = arr.find( rec => rec.result === "W" )
+  return results ? results.year : alert('undefined')
+}
+  
+
+var superbowlWin = (arr) => {
+  const results = arr.find( rec => rec.result === "W" )
+ 
+  if (results) {
+    return results.year
+  }
+}
